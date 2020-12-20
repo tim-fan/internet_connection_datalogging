008718eed0fa20ad2ddf005665bf64149eaa99c8
Tim <tim.fanselow@gmail.com>
2020-12-21 01:41:35 +1300

    add script for automatically pushing updated files

---
+++ scripts/push_updates.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# bash script to regularly `git add / git commit / git push` in a given directory
+# used to push log files to github
+
+if (( $# != 1 )); then
+    >&2 echo "Expected exactly one argument: LOG_DIR: directory to push"
+    exit 1
+fi
+
+LOG_DIR=$1
+
+cd $LOG_DIR
+
+while :
+do 
+    git add -u
+    git commit -m "Automatic update $(date)"
+    git push
+    sleep 300
+done
